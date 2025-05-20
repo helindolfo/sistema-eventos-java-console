6495a0e73c705fbd441141f364993a28e9f62d20
helindolfo <157731907+helindolfo@users.noreply.github.com>
2025-05-20 16:53:13 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,37 +1,42 @@
-// Usuario.java
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String nome;
     private String email;
     private String telefone;
-    private List<Evento> eventosInscritos = new ArrayList<>();
+    private List<Evento> eventosInscritos;
 
     public Usuario(String nome, String email, String telefone) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
+        this.eventosInscritos = new ArrayList<>();
     }
 
-    public void inscrever(Evento evento) {
-        if (!eventosInscritos.contains(evento)) {
-            eventosInscritos.add(evento);
-            evento.adicionarParticipante(this);
-        }
+    // Getters
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getTelefone() { return telefone; }
+    public List<Evento> getEventosInscritos() { return eventosInscritos; }
+
+    // Métodos para gerenciar eventos
+    public void inscreverEmEvento(Evento evento) {
+        eventosInscritos.add(evento);
     }
 
     public void cancelarInscricao(Evento evento) {
         eventosInscritos.remove(evento);
-        evento.removerParticipante(this);
     }
 
-    public List<Evento> getEventosInscritos() {
-        return eventosInscritos;
-    }
-
-    public String getNome() {
-        return nome;
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
     }
 }
